e69c079424b1cc063449486f57af47b31bf6c05f
Ricky Kuo <rickykuo925@gmail.com>
2022-03-14 12:10:21 +0000

    sml changes

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const popUp = document.getElementById('pop-up');
-const closeBtn = document.getElementById('close-btn');
 
 setTimeout(() => {
     popUp.classList.remove('hide');
     popUp.classList.add('show');
 }, 180000)
-
-closeBtn.addEventListener('click', () => window.location.replace("https://docs.google.com/forms/d/1OxL3iJySSax-Q7LIbOjGovNTYRIoHcdHgzkcEjNYENc/viewform?edit_requested=true"))
